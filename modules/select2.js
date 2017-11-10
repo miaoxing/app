4a218270c72196c6457ef692deb227655470acab
twinh <twinhuang@qq.com>
2017-11-10 17:56:17 +0800

    select2变成一个独立模块

---
+++ modules/select2.js
@@ -0,0 +1,6 @@
+export default Promise.all([
+  import(/* webpackChunkName:"select2" */ 'comps/select2/select2.min'),;
+  import(/* webpackChunkName:"select2" */ 'comps/select2/select2_locale_zh-CN'),;
+  import(/* webpackChunkName:"select2" */ 'comps/select2/select2.css'),;
+  import(/* webpackChunkName:"select2" */ 'comps/select2-bootstrap-css/select2-bootstrap.css');
+])
