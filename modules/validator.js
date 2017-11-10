202694d7aaf2da6baba6ef909289e2711db0f3ba
twinh <twinhuang@qq.com>
2017-11-10 17:58:45 +0800

    结尾不自动加上;

--- modules/validator.js
+++ modules/validator.js
@@ -1,6 +1,6 @@
 export default Promise.all([
-  import(/* webpackChunkName:"jquery-validate" */ 'comps/jquery-validation/jquery.validate'),;
-  import(/* webpackChunkName:"jquery-validate" */ 'comps/jquery-validation/localization/messages_zh');
+  import(/* webpackChunkName:"jquery-validate" */ 'comps/jquery-validation/jquery.validate'),
+  import(/* webpackChunkName:"jquery-validate" */ 'comps/jquery-validation/localization/messages_zh')
 ]).then(() => {
   import(/* webpackChunkName:"jquery-validation-bootstrap-tooltip" */ 'plugins/app/libs/jquery-validation-bootstrap-tooltip/jquery-validate.bootstrap-tooltip.min')
 });
