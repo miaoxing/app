1a7b01e2066a50417906589b84b0d67ba3dfb7ee
twinh <twinhuang@qq.com>
2017-11-17 15:43:22 +0800

    Content 改为更上一级的 Page

--- modules/components/Page.jsx
+++ modules/components/Page.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PageHeader from './PageHeader.jsx';
 
-class Content extends React.Component {
+class Page extends React.Component {
   render () {
     var firstChild = React.Children.toArray(this.props.children)[0];
 
     return (
-      <div className="content-wrapper">
+      <div className="page-wrapper">
         {firstChild.type !== PageHeader && <PageHeader />}
         {this.props.children}
       </div>
@@ -14,4 +14,4 @@ class Content extends React.Component {
   }
 }
 
-export default Content
+export default Page
