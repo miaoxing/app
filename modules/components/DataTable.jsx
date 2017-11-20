cb8a0a153176fea82025d37b1ee3fffaa7dc613b
twinh <twinhuang@qq.com>
2017-11-20 21:57:23 +0800

    确定出datatable的search和reload方法

--- modules/components/DataTable.jsx
+++ modules/components/DataTable.jsx
@@ -84,6 +84,41 @@ $.extend(true, $.fn.dataTable.defaults, {
   }
 });
 
+/**
+ * 传入字符串参数搜索DataTable
+ *
+ * 1. URL会更新
+ * 2. 会跳转到第一页
+ *
+ * @param setting
+ * @param params
+ */
+$.fn.dataTableExt.oApi.search = function (setting, params) {
+  // 记录原始的URL,每次搜索使用原始URL,避免参数不断叠加
+  if (typeof setting.ajax.origUrl == 'undefined') {
+    setting.ajax.origUrl = setting.ajax.url;
+  }
+
+  // 允许直接传入jQuery对象
+  if (params instanceof jQuery) {
+    params = params.serializeArray();
+  }
+
+  var table = this.DataTable();
+  var url = $.appendUrl(setting.ajax.origUrl, params);
+  table.ajax.url(url).load();
+};
+
+/**
+ * 重新加载当前页面
+ *
+ * 1. URL不会更新
+ * 2. 保留在当前页面
+ */
+$.fn.dataTableExt.oApi.reload = function () {
+  this.DataTable().ajax.reload(null, false);
+};
+
 class DataTable extends React.Component {
   render () {
     return (
