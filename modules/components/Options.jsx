e6e2721ae62beae9178cdcab69807f8114a9849f
twinh <twinhuang@qq.com>
2017-11-20 09:30:06 +0800

    Options组件优化

--- modules/components/Options.jsx
+++ modules/components/Options.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Options extends React.Component {
   render () {
     const options = [];
-    if (this.props.default) {
-      options.push(<option key="">{this.props.default}</option>)
+
+    if (this.props.placeholder) {
+      options.push(<option key="">{this.props.placeholder}</option>)
     }
 
     this.props.data.forEach((option) =>
-      options.push(<option key={option[this.props.value]}
-        value={option[this.props.value]}>{option[this.props.label]}</option>)
+      options.push(<option key={option[this.props.valueKey]}
+        value={option[this.props.valueKey]}>{option[this.props.labelKey]}</option>)
     );
 
     return options;
   }
 }
+
+Options.defaultProps = {
+  labelKey: 'label',
+  valueKey: 'value',
+  placeholder: '请选择'
+};
+
+Options.propTypes = {
+  data: PropTypes.array,
+  labelKey: PropTypes.string,
+  valueKey: PropTypes.string,
+  placeholder: PropTypes.string
+};
 
 export default Options
