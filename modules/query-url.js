18a0b84d58de5c3783efa07fe0e287c369d239ec
twinh <twinhuang@qq.com>
2017-11-21 10:29:06 +0800

    解决query-url加载错误

--- modules/query-url.js
+++ modules/query-url.js
@@ -1,4 +1,6 @@
-export default import('./jquery-deparam').then(function () {
+import jQueryDeparam from 'jquery-deparam';
+
+export default jQueryDeparam.then(function () {
   $.queryUrl = function (url, args) {
     var params = $.deparam(location.search.substring(1));
     return $.url(url, typeof args != 'undefined' ? args : params, args);
