f00ce3c8bffd32e8c0b6a38ef9c8ef1a82a6a944
twinh <twinhuang@qq.com>
2017-11-21 11:09:38 +0800

    增加deletable

--- modules/components/DataTable.jsx
+++ modules/components/DataTable.jsx
@@ -119,6 +119,28 @@ $.fn.dataTableExt.oApi.reload = function () {
   this.DataTable().ajax.reload(null, false);
 };
 
+/**
+ * 为dataTable增加点击链接删除记录功能
+ *
+ * @param setting
+ * @param linkSelector
+ * @param message
+ */
+$.fn.dataTableExt.oApi.deletable = function (setting, linkSelector, message) {
+  var $table = this;
+  linkSelector = linkSelector || '.js-delete-record';
+  message = message || '删除后将无法还原,确认删除?';
+  $table.on('click', linkSelector, function () {
+    var $link = $(this);
+    $.confirm(message, function () {
+      $.post($link.data('href'), function (ret) {
+        $.msg(ret);
+        $table.reload();
+      }, 'json');
+    });
+  });
+};
+
 class DataTable extends React.Component {
   render () {
     return (
