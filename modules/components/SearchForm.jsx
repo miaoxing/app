bc2a628b5ea93a8faf31a5e2f93d14970b90f302
twinh <twinhuang@qq.com>
2017-11-22 15:13:46 +0800

    SearchForm默认启用loadQuery功能

--- modules/components/SearchForm.jsx
+++ modules/components/SearchForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {findDOMNode} from 'react-dom';
+import PropTypes from 'prop-types';
 
 import Form from './Form.jsx';
+import loadQuery from 'load-query';
 import 'plugins/admin/css/filter.css';
 
 class SearchForm extends React.Component {
+  componentDidMount () {
+    if (this.props.loadQuery) {
+      loadQuery.then(() => $(findDOMNode(this)).loadQuery());
+    }
+  }
+
   render () {
     return (
       <Form horizontal className={this.props.className + ' search-form well'}>
@@ -12,5 +21,13 @@ class SearchForm extends React.Component {
     );
   }
 }
+
+SearchForm.defaultProps = {
+  loadQuery: true
+};
+
+SearchForm.propTypes = {
+  loadQuery: PropTypes.bool
+};
 
 export default SearchForm
