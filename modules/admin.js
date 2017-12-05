d7e57dc388a1efcbb8222bd522f6e07a94c53a51
twinh <twinhuang@qq.com>
2017-12-05 10:09:33 +0800

    入口移到 modules/containers 中

--- modules/admin.js
+++ modules/admin.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
 const map = {
-  'admin/question-experts': () => import(/* webpackChunkName:'question-experts' */ 'vendor/miaoxing/question/public/js/admin/question-experts/index.jsx'),
-  'admin/question-experts/new': () => import(/* webpackChunkName:'question-experts-form' */ 'vendor/miaoxing/question/public/js/admin/question-experts/form.jsx'),
-  'admin/groups': () => import(/* webpackChunkName:'groups' */ 'vendor/miaoxing/user/public/js/admin/groups/index.jsx'),
-  'admin/groups/new': () => import(/* webpackChunkName:'groups-form' */ 'vendor/miaoxing/user/public/js/admin/groups/form.jsx'),
+  'admin/question-experts': () => import(/* webpackChunkName:'question-experts' */ 'vendor/miaoxing/question/modules/containers/admin/question-experts/index.jsx'),
+  'admin/question-experts/new': () => import(/* webpackChunkName:'question-experts-form' */ 'vendor/miaoxing/question/modules/containers/admin/question-experts/form.jsx'),
+  'admin/groups': () => import(/* webpackChunkName:'groups' */ 'vendor/miaoxing/user/modules/containers/admin/groups/index.jsx'),
+  'admin/groups/new': () => import(/* webpackChunkName:'groups-form' */ 'vendor/miaoxing/user/modules/containers/admin/groups/form.jsx'),
 }
 
 const load = () => {
