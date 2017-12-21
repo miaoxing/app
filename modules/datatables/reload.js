a0e2617a0ab74db80b2421cef4745cae405f0e2f
twinh <twinhuang@qq.com>
2017-12-21 10:44:26 +0800

    解决参数错误导致重载出错

--- modules/datatables/reload.js
+++ modules/datatables/reload.js
@@ -4,6 +4,6 @@
  * 1. URL不会更新
  * 2. 保留在当前页面
  */
-$.fn.dataTableExt.oApi.reload = function (callback) {
+$.fn.dataTableExt.oApi.reload = function (setting, callback) {
   this.DataTable().ajax.reload(callback, false);
 };
