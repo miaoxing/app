c0a318345e1fc732b75ab1e2fd6bc4e73fe518b4
twinh <twinhuang@qq.com>
2018-01-02 16:13:37 +0800

    生成文档移除多余空格 #3793

--- docs/AutoComplete.php
+++ docs/AutoComplete.php
@@ -3,30 +3,30 @@
 namespace MiaoxingDoc\App {
 
     /**
-     * @property    \Miaoxing\App\Service\Asset $asset 
-     * @method      mixed asset($file, $version = true) 
+     * @property    \Miaoxing\App\Service\Asset $asset
+     * @method      mixed asset($file, $version = true)
      * @see         \Miaoxing\App\Service\Asset::__invoke
      *
      * @property    \Miaoxing\App\Service\Captcha $captcha 图形验证码
      *
-     * @property    \Miaoxing\App\Service\Coll $coll 
+     * @property    \Miaoxing\App\Service\Coll $coll
      *
-     * @property    \Miaoxing\App\Service\Logger $logger 
+     * @property    \Miaoxing\App\Service\Logger $logger
      * @method      bool logger($level, $message, $context = []) Logs with an arbitrary level
      * @see         \Miaoxing\App\Service\Logger::__invoke
      *
      * @property    \Miaoxing\App\Service\Page $page 页面
      *
-     * @property    \Miaoxing\App\Service\Random $random 
+     * @property    \Miaoxing\App\Service\Random $random
      *
-     * @property    \Miaoxing\App\Service\Seq $seq 
-     * @method      null|int seq($offset = 100) 
+     * @property    \Miaoxing\App\Service\Seq $seq
+     * @method      null|int seq($offset = 100)
      * @see         \Miaoxing\App\Service\Seq::__invoke
      *
      * @property    \Miaoxing\App\Service\Status $status 状态
      *
-     * @property    \Miaoxing\App\Service\UserApp $userApp 
-     * @method      \Miaoxing\App\Service\UserApp|\Miaoxing\App\Service\UserApp[] userApp() 
+     * @property    \Miaoxing\App\Service\UserApp $userApp
+     * @method      \Miaoxing\App\Service\UserApp|\Miaoxing\App\Service\UserApp[] userApp()
      * @see         \Miaoxing\App\Service\UserApp::__invoke
      */
     class AutoComplete
