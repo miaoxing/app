278820661f97a16e6e7b7e32fd6996f1bae522f8
twinh <twinhuang@qq.com>
2018-01-02 17:30:59 +0800

    解决eslint问题 #3794

--- modules/admin.js
+++ modules/admin.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
 
 // 允许外部根据环境配置目录，而不是webpack.config.js中写死
 if (wei.webpackPublicPath) {
-  __webpack_public_path__ = wei.webpackPublicPath
+  // eslint-disable-next-line camelcase,no-undef
+  __webpack_public_path__ = wei.webpackPublicPath;
 }
 
 const load = () => {
   import(/* webpackChunkName:'react-containers' */'data/react-containers').then(containers => {
     if (!containers.default[wei.reactContainer]) {
-      throw 'React container "' + wei.reactContainer + '" not found';
+      throw new Error('React container "' + wei.reactContainer + '" not found');
     }
 
     containers.default[wei.reactContainer]().then((Root) => {
-      render(Root.default)
-    })
-  })
+      render(Root.default);
+    });
+  });
 };
 
 const render = Component => {
@@ -25,12 +26,12 @@ const render = Component => {
       <Component/>
     </AppContainer>,
     document.getElementById('root')
-  )
+  );
 };
 
 load();
 if (module.hot) {
   module.hot.accept(() => {
-    load()
-  })
+    load();
+  });
 }
--- modules/auto-import-name.js
+++ modules/auto-import-name.js
@@ -1,26 +1,28 @@
 module.exports = function () {
-
-  const visited = Symbol('visited')
-  const getImportArgPath = p => p.parentPath.get('arguments')[0]
-  const trimChunkName = baseDir => baseDir.replace(/^[./]+|(\.js$)/g, '')
+  // eslint-disable-next-line no-undef
+  const visited = Symbol('visited');
+  const getImportArgPath = p => p.parentPath.get('arguments')[0];
+  const trimChunkName = baseDir => baseDir.replace(/^[./]+|(\.js$)/g, '');
 
   function getMagicCommentChunkName(importArgNode) {
-    const { quasis, expressions } = importArgNode
-    if (!quasis) return trimChunkName(importArgNode.value)
+    const {quasis, expressions} = importArgNode;
+    if (!quasis) {
+      return trimChunkName(importArgNode.value);
+    }
 
-    const baseDir = quasis[0].value.cooked
-    const hasExpressions = expressions.length > 0
-    const chunkName = baseDir + (hasExpressions ? '[request]' : '')
-    return trimChunkName(chunkName)
+    const baseDir = quasis[0].value.cooked;
+    const hasExpressions = expressions.length > 0;
+    const chunkName = baseDir + (hasExpressions ? '[request]' : '');
+    return trimChunkName(chunkName);
   }
 
   function promiseAll(p) {
-    const argPath = getImportArgPath(p)
-    const importArgNode = argPath.node
-    const chunkName = getMagicCommentChunkName(importArgNode)
+    const argPath = getImportArgPath(p);
+    const importArgNode = argPath.node;
+    const chunkName = getMagicCommentChunkName(importArgNode);
 
     if (!argPath.node.leadingComments) {
-      argPath.addComment('leading', ` webpackChunkName: '${chunkName}' `)
+      argPath.addComment('leading', ` webpackChunkName: '${chunkName}' `);
     }
   }
 
@@ -28,10 +30,12 @@ module.exports = function () {
     name: 'auto-import-name',
     visitor: {
       Import(p) {
-        if (p[visited]) return
-        p[visited] = true
-        promiseAll(p)
+        if (p[visited]) {
+          return;
+        }
+        p[visited] = true;
+        promiseAll(p);
       }
     }
-  }
-}
+  };
+};
