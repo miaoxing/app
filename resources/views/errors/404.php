aa5afad729bcd98ad9038f8d5cb7459ab9e4baff
twinh <twinhuang@qq.com>
2018-02-04 00:28:42 +0800

    解决 ag attributes are malformed #3821\nresources/views/errors/404.php: line 5, col 3, tag attributes are malformed\nresources/views/errors/500.php: line 5, col 3, tag attributes are malformed

--- resources/views/errors/404.php
+++ resources/views/errors/404.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>页面不存在</title>
 </head>
 <body>
