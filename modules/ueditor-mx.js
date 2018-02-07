0df8a68df29cfcc4960a78a3567647434dda0d43
twinh <twinhuang@qq.com>
2018-02-07 14:25:07 +0800

    ueditor 和 form 解耦

--- modules/ueditor-mx.js
+++ modules/ueditor-mx.js
@@ -1,12 +1,3 @@
 import 'vendor/miaoxing/ueditor/modules/ueditor-config';
 import 'comps/ueditor/ueditor.all.min';
 import 'comps/ueditor/lang/zh-cn/zh-cn';
-
-$.fn.ueditor = function () {
-  const id = $(this).attr('id');
-  /* global UE */
-  const editor = new UE.ui.Editor();
-  editor.render(id);
-
-  return this;
-};
