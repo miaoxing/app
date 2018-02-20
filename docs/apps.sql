fae3d33379064f6ae4aa77b226a13c41d97641c9
twinh <twinhuang@qq.com>
2018-02-20 11:38:52 +0800

    还原SQL

--- docs/apps.sql
+++ docs/apps.sql
@@ -9,7 +9,6 @@ CREATE TABLE `app`.`apps` (
   `domain` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `industry` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '行业',
-  `status` tinyint(3) unsigned NOT NULL DEFAULT '1',
   `configs` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `createTime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updateTime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -19,7 +18,5 @@ CREATE TABLE `app`.`apps` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 BEGIN;
-INSERT INTO `app`.`apps` VALUES
-  ('1', '0', '', 'app', 'app', '', '', '', 'site', '1', '[]', '0000-00-00 00:00:00', '2016-07-22 15:51:57', '1', '1'),
-  ('2', '0', '', 'test', 'test', '', '', '', 'site', '1', '[]', '2015-04-02 23:42:04', '2016-06-16 14:51:42', '1', '1');
+INSERT INTO `app`.`apps` VALUES ('1', '0', '', 'app', 'app', '', '', '', 'site', '[]', '0000-00-00 00:00:00', '2016-07-22 15:51:57', '1', '1'), ('2', '0', '', 'test', 'test', '', '', '', 'site', '[]', '2015-04-02 23:42:04', '2016-06-16 14:51:42', '1', '1');
 COMMIT;
