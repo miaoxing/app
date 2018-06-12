f882a8df3d96a5195255ee58113de99b68e98e41
twinh <twinhuang@qq.com>
2018-06-12 09:31:09 +0800

    refactor(tabs): js-tabs类移到tabs中

--- modules/components/Tabs.jsx
+++ modules/components/Tabs.jsx
@@ -10,7 +10,7 @@ class Tabs extends React.Component {
   }
 
   render() {
-    return <RbTabs {...this.props}/>
+    return <RbTabs {...this.props} className={this.props.className + ' js-tabs'}/>
   }
 }
 
