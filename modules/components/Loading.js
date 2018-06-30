55875999a6ffe89c77a3bcbf6bea89c3ef05a851
twinh <twinhuang@qq.com>
2018-06-30 22:45:54 +0800

    feat: 指定后后缀来减少范围

--- modules/components/Loading.js
+++ modules/components/Loading.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default (props) => {
-  console.log(props.error);
   if (props.error) {
     return <div>Error! <button onClick={ props.retry }>Retry</button>
       <div>{props.error.toString()}</div>
