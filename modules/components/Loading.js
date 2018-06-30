ea9c541e9ce82a63ff0e354eb1692b8e04cd0487
twinh <twinhuang@qq.com>
2018-06-30 22:42:17 +0800

    feat: 完善loading

--- modules/components/Loading.js
+++ modules/components/Loading.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
-export default () => <div>Loading...</div>;
+export default (props) => {
+  console.log(props.error);
+  if (props.error) {
+    return <div>Error! <button onClick={ props.retry }>Retry</button>
+      <div>{props.error.toString()}</div>
+    </div>;
+  } else {
+    return <div>Loading...</div>;
+  }
+}
