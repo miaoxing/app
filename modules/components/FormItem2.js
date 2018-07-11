33e0da3033f79bc82a2a8cdfcfbf3699119bae01
twinh <twinhuang@qq.com>
2018-07-11 21:16:34 +0800

    feat: 先解决设置componentClass无效

--- modules/components/FormItem2.js
+++ modules/components/FormItem2.js
@@ -3,10 +3,7 @@ import FormItem from './FormItem.jsx';
 import {Field} from "formik";
 
 function FormItem2(props) {
-  if (!props.componentClass) {
-    props.componentClass = Field;
-  }
-  return <FormItem {...props}/>
+  return <FormItem componentClass={Field} {...props}/>
 }
 
 export default FormItem2;
