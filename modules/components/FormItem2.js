b53340ee96197a567691202f4c3dc881ab8ac9c8
twinh <twinhuang@qq.com>
2018-07-13 23:35:28 +0800

    feat: 自动识别出 component 为 select

--- modules/components/FormItem2.js
+++ modules/components/FormItem2.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import FormItem from './FormItem.jsx';
 import {Field} from "formik";
+import Options from "components/Options.jsx";
 
-function FormItem2(props) {
-  return <FormItem componentClass={Field} {...props}/>
+function FormItem2({...props}) {
+  // 自动识别select类型
+  if (!props.component) {
+    const firstChild = React.Children.toArray(props.children)[0];
+    if (firstChild && (firstChild.type === 'option' || firstChild.type === <Options/>.type)) {
+      props.component = 'select';
+    }
+  }
+
+  if (!props.componentClass) {
+    props.componentClass = Field;
+  }
+
+  return <FormItem {...props}/>
 }
 
 export default FormItem2;
