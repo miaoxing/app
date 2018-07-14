86860a883cbc34eb331110a579fad6751016b082
twinh <twinhuang@qq.com>
2018-07-14 19:22:32 +0800

    feat: 增加 ListBtn 组件

--- modules/components/FormAction2.js
+++ modules/components/FormAction2.js
@@ -11,13 +11,11 @@ function FormAction2({url}) {
     <FormGroup className="clearfix form-actions">
       <Col smOffset={2}>
         <Button bsStyle="primary" type="submit">
-          <i className="fa fa-check bigger-110"/>
-          {' '}提交
+          提交
         </Button>
         &nbsp; &nbsp; &nbsp;
         <BtnLink to={url}>
-          <i className="fa fa-undo bigger-110"/>
-          {' '}返回列表
+          返回列表
         </BtnLink>
       </Col>
     </FormGroup>
---
+++ modules/components/ListBtn.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import BtnLink from "components/BtnLink";
+
+export default (props) => <BtnLink to={props.href}>{props.children || '返回列表'}</BtnLink>
