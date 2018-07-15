2f947eb4091a1e81c9a01af7866ac53d674b85ed
twinh <twinhuang@qq.com>
2018-07-15 16:18:12 +0800

    feat: 整理组件，合并到components中

--- modules/components.js
+++ modules/components.js
@@ -1,14 +1,42 @@
+// 页面
 export Page from './components/Page.jsx';
-export FormItem from './components/FormItem.jsx';
-export FormAction from './components/FormAction.jsx';
-export Required from './components/Required.jsx';
 export PageHeader from './components/PageHeader.jsx';
+export NoMatch from './components/NoMatch';
+export Loading from './components/Loading';
+
+// 列表
 export DataTable from './components/DataTable.jsx';
+export DataTable2 from './components/DataTable2';
+export Actions from './components/Actions';
+
+// 表单
 export Form from './components/Form.jsx';
-export SearchForm from './components/SearchForm.jsx';
+export Form2 from './components/Form2';
+export FormAction from './components/FormAction.jsx';
+export FormAction2 from './components/FormAction2';
+export FormItem from './components/FormItem.jsx';
+export FormItem2 from './components/FormItem2';
 export Options from './components/Options.jsx';
-export SearchItem from './components/SearchItem.jsx';
+export Required from './components/Required.jsx';
 export ImageUpload from './components/ImageUpload.jsx';
+
+export SearchForm from './components/SearchForm.jsx';
+export SearchForm2 from './components/SearchForm2';
+export SearchItem from './components/SearchItem.jsx';
+
 export Tabs from './components/Tabs.jsx';
-export Img from './components/Img.jsx';
 export TabNavs from './components/TabNavs.jsx';
+
+export Img from './components/Img.jsx';
+export BtnLink from './components/BtnLink';
+
+export ListBtn from './components/ListBtn';
+export NewBtn from './components/NewBtn';
+export EditLink from './components/EditLink';
+export DeleteLink from './components/DeleteLink';
+
+// 业务相关的
+export CDeleteLink from './components/CDeleteLink';
+export CEditLink from './components/CEditLink';
+export CListBtn from './components/CListBtn';
+export CNewBtn from './components/CNewBtn';
