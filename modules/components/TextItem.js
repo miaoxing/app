64856de44cc8f99e8d5e14759feb6f849c36cdd1
twinh <twinhuang@qq.com>
2018-08-10 22:45:03 +0800

    feat: 完善人资管理员查看某个年度协议的详情

---
+++ modules/components/TextItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Col, ControlLabel, FormGroup, FormControl} from "react-bootstrap";
+
+const TextItem = ({label, children}) => {
+  return <FormGroup>
+    <Col componentClass={ControlLabel} sm={2}>
+      {label}
+    </Col>
+    <Col sm={10}>
+      <FormControl.Static>{children || '-'}</FormControl.Static>
+    </Col>
+  </FormGroup>;
+};
+
+export default TextItem;
