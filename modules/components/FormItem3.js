c4ea7f3e708883829a7c59a44dfb8fd8ce20fe90
twinh <twinhuang@qq.com>
2018-08-14 20:07:05 +0800

    feat: 初始化上报操作

--- modules/components/FormItem3.js
+++ modules/components/FormItem3.js
@@ -2,15 +2,20 @@ import React from "react";
 import {ControlLabel, FormControl, FormGroup} from "react-bootstrap";
 import {Field} from "formik";
 import Required from "components/Required.jsx";
+import decamelize from "decamelize";
+import trim from "trim-character";
 
-const FormItem3 = ({id, label, ...props}) => {
+const FormItem3 = ({name = '', label, ...props}) => {
+  // 移除 name[] 后面的 []
+  const id = trim(decamelize(name.replace(/\[\]/g, '-'), '-'), '-');
+
   return <FormGroup controlId={id}>
     <ControlLabel>
       {label}
       {props.required && <React.Fragment>{' '}<Required/></React.Fragment>}
     </ControlLabel>
     <div className="col-control">
-      <FormControl componentClass={Field} {...props} />
+      <FormControl componentClass={Field} id={id} name={name} {...props} />
     </div>
   </FormGroup>
 };
