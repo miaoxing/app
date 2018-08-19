c5015cf1f3f68505055053145e47488762ad7c1a
twinh <twinhuang@qq.com>
2018-08-19 11:16:30 +0800

    feat: 实现checkbox操作

---
+++ modules/components/Checkbox.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Field} from "formik";
+
+/**
+ * @link https://codesandbox.io/s/328038x19q
+ */
+function Checkbox(props) {
+  return <Field name={props.name}>
+    {({field, form}) => <input
+      type="checkbox"
+      {...props}
+      checked={field.value.includes(props.value)}
+      onChange={() => {
+        if (field.value.includes(props.value)) {
+          const nextValue = field.value.filter(
+            value => value !== props.value
+          );
+          form.setFieldValue(props.name, nextValue);
+        } else {
+          const nextValue = field.value.concat(props.value);
+          form.setFieldValue(props.name, nextValue);
+        }
+      }}
+    />
+    }
+  </Field>;
+}
+
+export default Checkbox;
