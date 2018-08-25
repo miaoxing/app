9b81a5a13ed357bf85249f29f2976bd68ebee194
twinh <twinhuang@qq.com>
2018-08-25 21:08:38 +0800

    feat: 增加手机验证码相关操作

---
+++ modules/components/MobileVerifyCode.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'plugins/verify-code/js/verify-code'
+import {Field} from "formik";
+
+class MobileVerifyCode extends React.Component {
+  componentDidUpdate() {
+    $('.js-verify-code-send').verifyCode();
+  }
+
+  render() {
+    if (this.props.defaultMobile && this.props.defaultMobile === this.props.mobile) {
+      return '';
+    }
+
+    return <div className="form-group">
+      <label htmlFor="verify-code" className="control-label">
+        验证码
+        <span className="text-warning">*</span>
+      </label>
+      <div className="col-control">
+        <div className="input-group">
+          <Field type="tel" className="form-control" id="verify-code" name="verifyCode" placeholder="请输入验证码"/>
+          <span className="input-group-btn border-left">
+            <button type="button" className="js-verify-code-send text-primary btn btn-default form-link">
+              获取验证码
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export default MobileVerifyCode;
