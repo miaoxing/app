88e4c7555095896ba63ef9cba7c289c3942fea6c
twinh <twinhuang@qq.com>
2018-08-26 15:54:46 +0800

    feat: 整理出SearchDateRangePicker组件

---
+++ modules/components/SearchDateRangePicker.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import rp from "require-promise";
+import SearchItem from "components/SearchItem.jsx";
+import decamelize from 'decamelize';
+
+const loader = Promise.all([
+  rp('daterangepicker'),
+]);
+
+class SearchDateRangePicker extends React.Component {
+  id = decamelize(this.props.name, '-');
+
+  componentDidMount() {
+    loader.then(() => {
+      $('.js-' + this.id).daterangepicker({
+        format: 'YYYY-MM-DD',
+        separator: ' ~ '
+      }, function (start, end) {
+        $('.js-' + this.element.attr('id') + '-min').val(start.format(this.format) + ' 00:00:00');
+        $('.js-' + this.element.attr('id') + '-max').val(end.format(this.format) + ' 23:59:59');
+        this.element.trigger('change');
+      });
+    });
+  }
+
+  render() {
+    return <React.Fragment>
+      <SearchItem label={this.props.label} className={'js-' + this.id} name={this.props.name} autocomplete="off"/>
+      <input type="hidden" className={'js-' + this.id + '-min'} name={this.props.name + 'Min'}/>
+      <input type="hidden" className={'js-' + this.id + '-max'} name={this.props.name + 'Max'}/>
+    </React.Fragment>
+  }
+}
+
+export default SearchDateRangePicker;
