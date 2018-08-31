b0f0b32969123d490e0a2ca4331599fea95b46b6
twinh <twinhuang@qq.com>
2018-08-31 16:31:49 +0800

    feat: 完善 loading 样式

--- modules/components/Loading.js
+++ modules/components/Loading.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default (props) => {
   if (props.error) {
-    return <div>很抱歉,加载出错! <button className="btn btn-default btn-sm" onClick={ props.retry }>重试</button>
+    return <div className="m-t">很抱歉,加载出错! <button className="btn btn-default btn-sm" onClick={ props.retry }>重试</button>
       <div className="hide">{props.error.toString()}</div>
     </div>;
   } else {
-    return <div className="text-center">努力加载中...</div>;
+    return <div className="text-center m-t">努力加载中...</div>;
   }
 }
