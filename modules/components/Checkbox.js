088043ea67c94eb832dabef2c8f2ee63a218c930
twinh <twinhuang@qq.com>
2018-08-31 18:03:01 +0800

    chore: build asset

--- modules/components/Checkbox.js
+++ modules/components/Checkbox.js
@@ -6,24 +6,23 @@ import {Field} from "formik";
  */
 function Checkbox(props) {
   return <Field name={props.name}>
-    {({field, form}) => {
-      return <input
-        type="checkbox"
-        {...props}
-        checked={field.value.includes(props.value)}
-        onChange={() => {
-          if (field.value.includes(props.value)) {
-            const nextValue = field.value.filter(
-              value => value !== props.value
-            );
-            form.setFieldValue(props.name, nextValue);
-          } else {
-            const nextValue = field.value.concat(props.value);
-            form.setFieldValue(props.name, nextValue);
-          }
-        }}
-      />
-    }}
+    {({field, form}) => <input
+      type="checkbox"
+      {...props}
+      checked={field.value.includes(props.value)}
+      onChange={() => {
+        if (field.value.includes(props.value)) {
+          const nextValue = field.value.filter(
+            value => value !== props.value
+          );
+          form.setFieldValue(props.name, nextValue);
+        } else {
+          const nextValue = field.value.concat(props.value);
+          form.setFieldValue(props.name, nextValue);
+        }
+      }}
+    />
+    }
   </Field>;
 }
 
