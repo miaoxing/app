e894fc3bee3925560a5a931b54c303b06378ceb8
twinh <twinhuang@qq.com>
2018-09-05 15:04:33 +0800

    Merge branch 'master' into project/lfy\n\n# Conflicts:\n#\tdata/cache/plugins-config.php\n#\tvendor/miaoxing/app/modules/components/MobileVerifyCode.js

--- modules/components/Checkbox.js
+++ modules/components/Checkbox.js
@@ -6,23 +6,24 @@ import {Field} from "formik";
  */
 function Checkbox(props) {
   return <Field name={props.name}>
-    {({field, form}) => <input
-      type="checkbox"
-      {...props}
-      checked={field.value.includes(props.value)}
-      onChange={() => {
-        if (field.value.includes(props.value)) {
-          const nextValue = field.value.filter(
-            value => value !== props.value
-          );
-          form.setFieldValue(props.name, nextValue);
-        } else {
-          const nextValue = field.value.concat(props.value);
-          form.setFieldValue(props.name, nextValue);
-        }
-      }}
-    />
-    }
+    {({field, form}) => {
+      return <input
+        type="checkbox"
+        {...props}
+        checked={field.value.includes(props.value)}
+        onChange={() => {
+          if (field.value.includes(props.value)) {
+            const nextValue = field.value.filter(
+              value => value !== props.value
+            );
+            form.setFieldValue(props.name, nextValue);
+          } else {
+            const nextValue = field.value.concat(props.value);
+            form.setFieldValue(props.name, nextValue);
+          }
+        }}
+      />
+    }}
   </Field>;
 }
 
