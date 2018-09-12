cd51681461dc4e6412a49fed0f70be73303ebf24
twinh <twinhuang@qq.com>
2018-09-12 15:07:26 +0800

    test: 新增岗位时，最下方的申请改成浮动

---
+++ modules/components/FooterBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class FooterBar extends React.Component {
+  render() {
+    return <div className="footer-bar footer-bar-fluid flex">
+      <style>
+        {`
+        body {
+            margin-bottom: ${this.props.height}px;
+        }
+        `}
+      </style>
+      {this.props.children}
+    </div>
+  }
+}
+
+FooterBar.defaultProps = {
+  height: 54 // 44 + 10
+};
+
+export default FooterBar;
