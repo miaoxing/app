ea44fac9bef342af8ef711fe1a1849c646336a33
twinh <twinhuang@qq.com>
2018-10-09 16:52:49 +0800

    feat: TabNavs 支持定义类名

--- modules/components/TabNavs.js
+++ modules/components/TabNavs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import _ from 'lodash';
+import classNames from 'classnames';
 
 class TabNavs extends React.Component {
   render() {
@@ -8,7 +9,7 @@ class TabNavs extends React.Component {
     const cur = item ? item.key : 'all';
     const baseUrl = this.props.baseUrl || window.location.pathname;
 
-    return <ul className="header-tab nav tab-underline border-top-bottom">
+    return <ul className={classNames('header-tab nav tab-underline border-top-bottom', this.props.className)}>
       {_.map(this.props.data, (item) => {
         return <li key={item.id} className={'border-primary ' + (cur === item.key ? 'active' : '')}>
           <Link to={$.appendUrl(baseUrl, {[this.props.paramName]: item.key})}
