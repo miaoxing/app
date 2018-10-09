0da6a41fe2e979552640dbb0f82628dc5012275a
twinh <twinhuang@qq.com>
2018-10-09 18:05:57 +0800

    feat: TabNavs 尝试不显示key=all 可以自动增加 all

--- modules/components/TabNavs.js
+++ modules/components/TabNavs.js
@@ -5,14 +5,21 @@ import classNames from 'classnames';
 
 class TabNavs extends React.Component {
   render() {
+    if (this.props.all) {
+      this.props.data[0] = {
+        key: 'all',
+        name: '全部'
+      };
+    }
+
     const item = _.find(this.props.data, ['key', $.req(this.props.paramName)]);
     const cur = item ? item.key : 'all';
     const baseUrl = this.props.baseUrl || window.location.pathname;
 
     return <ul className={classNames('header-tab nav tab-underline border-top-bottom', this.props.className)}>
       {_.map(this.props.data, (item) => {
-        return <li key={item.id} className={'border-primary ' + (cur === item.key ? 'active' : '')}>
-          <Link to={$.appendUrl(baseUrl, {[this.props.paramName]: item.key})}
+        return <li key={item.key} className={'border-primary ' + (cur === item.key ? 'active' : '')}>
+          <Link to={$.appendUrl(baseUrl, item.key === 'all' ? {} : {[this.props.paramName]: item.key})}
             className="text-active-primary">{item.name}</Link>
         </li>;
       })}
@@ -21,6 +28,7 @@ class TabNavs extends React.Component {
 }
 
 TabNavs.defaultProps = {
+  all: false,
   data: [],
   paramName: 'status'
 };
