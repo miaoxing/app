183bbda794c485b0207951e773822d8fa89d4fbb
twinh <twinhuang@qq.com>
2018-10-10 14:35:15 +0800

    refactor: DeleteLink 改为需传入tableRef以便刷新

--- modules/components/DeleteLink.js
+++ modules/components/DeleteLink.js
@@ -16,22 +16,23 @@ class DeleteLink extends React.Component {
       }).done(ret => {
         $.msg(ret);
         if (ret.code === 1) {
-          $(document).trigger('tableReload');
+          this.props.tableRef.current.reload();
         }
       });
     });
   }
 
   render() {
-    const {message, href, ...rest} = this.props;
-    return <a className="text-danger" href="javascript:" onClick={this.handleDelete} {...rest}>
+    const {message, href, tableRef, ...rest} = this.props;
+    return <a className="text-danger" onClick={this.handleDelete} {...rest}>
       删除
     </a>;
   }
 }
 
 DeleteLink.defaultProps = {
-  message: '删除后将无法还原,确认删除?'
+  message: '删除后将无法还原,确认删除?',
+  tableRef: null,
 };
 
 export default DeleteLink;
