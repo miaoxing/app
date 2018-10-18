1cef1da8a815fe7d21225b2f59798acfa318f649
twinh <twinhuang@qq.com>
2018-10-18 22:04:43 +0800

    feat: FormAction 可以不显示返回按钮

--- modules/components/FormAction.js
+++ modules/components/FormAction.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, Col, FormGroup} from 'react-bootstrap';
 import CListBtn from 'components/CListBtn';
 
-function FormAction({url}) {
+function FormAction({url, list = true}) {
   return (
     <FormGroup className="clearfix form-actions">
       <Col smOffset={2}>
@@ -10,10 +10,10 @@ function FormAction({url}) {
           提交
         </Button>
         &nbsp; &nbsp; &nbsp;
-        {url ? <Button href={url}>
+        {list && (url ? <Button href={url}>
           <i className="fa fa-undo bigger-110"/>
           {' '}返回列表
-        </Button> : <CListBtn/>}
+        </Button> : <CListBtn/>)}
       </Col>
     </FormGroup>
   );
