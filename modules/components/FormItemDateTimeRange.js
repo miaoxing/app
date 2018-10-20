dc7d59fe12e7bb96cbdbb40adfc5ea365675fbbf
twinh <twinhuang@qq.com>
2018-10-20 18:29:27 +0800

    feat: 整理日期范围的值,由formik管理

--- modules/components/FormItemDateTimeRange.js
+++ modules/components/FormItemDateTimeRange.js
@@ -13,51 +13,48 @@ class FormItemDateTimeRange extends React.Component {
     maxName: 'endedAt'
   };
 
-  constructor(props, context) {
-    super(props, context);
-
-    const values = props.formik.values;
-    this.state = {
-      dateMin: values[this.props.minName] ? moment(values[this.props.minName]) : null,
-      dateMax: values[this.props.maxName] ? moment(values[this.props.maxName]) : null,
-    };
-  }
-
   handleChangeStart(dateMin) {
-    const dateValue = dateMin ? dateMin.format('YY-MM-DD HH:mm:ss') : '';
-    let state = {dateMin};
+    const dateValue = this.format(dateMin);
+    const dateMax = this.moment(this.props.formik.values[this.props.dateMax]);
 
-    if (dateMin && dateMin.isAfter(this.state.dateMax)) {
-      state.dateMax = dateMin;
+    if (dateMin && dateMin.isAfter(dateMax)) {
       this.props.formik.setFieldValue(this.props.maxName, dateValue);
     }
 
-    this.setState(state);
     this.props.formik.setFieldValue(this.props.minName, dateValue);
   }
 
   handleChangeEnd(dateMax) {
-    const dateValue = dateMax ? dateMax.format('YY-MM-DD HH:mm:ss') : '';
-    let state = {dateMax};
+    const dateValue = this.format(dateMax);
+    const dateMin = this.moment(this.props.formik.values[this.props.dateMin]);
 
-    if (dateMax && dateMax.isBefore(this.state.dateMin)) {
-      state.dateMin = dateMax;
+    if (dateMax && dateMax.isBefore(dateMin)) {
       this.props.formik.setFieldValue(this.props.minName, dateValue);
     }
 
-    this.setState(state);
     this.props.formik.setFieldValue(this.props.maxName, dateValue);
   }
 
+  format(value) {
+    return value ? value.format('YYYY-MM-DD HH:mm:ss') : '';
+  }
+
+  moment(value) {
+    return value ? moment(value) : null;
+  }
+
   render() {
+    const dateMin = this.moment(this.props.formik.values[this.props.minName]);
+    const dateMax = this.moment(this.props.formik.values[this.props.maxName]);
+
     return <React.Fragment>
       <FormItem
         label={'开始' + this.props.label + '时间'}
         control={<DatePicker
-          selected={this.state.dateMin}
+          selected={dateMin}
           selectsStart
-          startDate={this.state.dateMin}
-          endDate={this.state.dateMax}
+          startDate={dateMin}
+          endDate={dateMax}
           onChange={this.handleChangeStart.bind(this)}
           name={this.props.minName}
           dateFormat="YYYY-MM-DD HH:mm:00"
@@ -71,10 +68,10 @@ class FormItemDateTimeRange extends React.Component {
       <FormItem
         label={'结束' + this.props.label + '时间'}
         control={<DatePicker
-          selected={this.state.dateMax}
+          selected={dateMax}
           selectsEnd
-          startDate={this.state.dateMin}
-          endDate={this.state.dateMax}
+          startDate={dateMin}
+          endDate={dateMax}
           onChange={this.handleChangeEnd.bind(this)}
           name={this.props.maxName}
           dateFormat="YYYY-MM-DD HH:mm:00"
