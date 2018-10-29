923437931cfc51966071c9069994f0d94f9eb80d
twinh <twinhuang@qq.com>
2018-10-29 17:02:58 +0800

    feat: 管理员查看经销者列表

--- modules/components/SearchDateRangePicker.js
+++ modules/components/SearchDateRangePicker.js
@@ -31,7 +31,8 @@ class SearchDateRangePicker extends React.Component {
 
   render() {
     return <React.Fragment>
-      <SearchItem label={this.props.label} className={'js-' + this.id} autoComplete="off"/>
+      <SearchItem label={this.props.label} className={'js-' + this.id} autoComplete="off"
+        name={this.props.name + 'Range'}/>
       <input type="hidden" className={'js-' + this.id + '-min'} name={this.props.name + this.props.min}/>
       <input type="hidden" className={'js-' + this.id + '-max'} name={this.props.name + this.props.max}/>
     </React.Fragment>;
