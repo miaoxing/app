cff9750963415f68881e60d64ae32104d74df74a
twinh <twinhuang@qq.com>
2018-11-06 16:14:34 +0800

    fix: 解决编辑时,输入框没有显示内容

--- modules/components/FormItemUeditor.js
+++ modules/components/FormItemUeditor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormItem from "components/FormItem";
-import {connect} from "formik";
+import {connect, getIn} from "formik";
 import rp from 'require-promise';
 
 const loader = rp('ueditor');
@@ -8,12 +8,27 @@ const loader = rp('ueditor');
 class FormItemUeditor extends React.Component {
   editor;
 
+  componentDidUpdate(prevProps) {
+    if (!this.editor) {
+      return;
+    }
+
+    if (getIn(prevProps.formik.values, this.props.name) !== getIn(this.props.formik.values, this.props.name)) {
+      this.editor.setContent(getIn(this.props.formik.values, this.props.name));
+    }
+  }
+
   componentDidMount() {
-    loader.then(() => {
-      this.editor = UE.getEditor(this.props.name);
+    this.getEditor().then(() => {
       this.editor.addListener('contentChange', () => {
         this.props.formik.setFieldValue(this.props.name, this.editor.getContent());
       });
+    });
+  }
+
+  getEditor() {
+    return loader.then(() => {
+      this.editor = UE.getEditor(this.props.name);
     });
   }
 
