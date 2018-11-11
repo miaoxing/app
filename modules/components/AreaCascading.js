ba895ca5a22cc7021bceecd0cd2c15b932e78450
twinh <twinhuang@qq.com>
2018-11-11 22:21:11 +0800

    feat: 加载顺序可能导致加载不完全

--- modules/components/AreaCascading.js
+++ modules/components/AreaCascading.js
@@ -20,17 +20,17 @@ class AreaCascading extends React.Component {
   };
 
   componentDidMount() {
-    this.get().then(ret => this.setState({provinces: ret.data}));
-
-    const province = getIn(this.props.formik.values, this.props.provinceName);
-    if (province) {
-      this.loadCities(province);
-    }
-
-    const city = getIn(this.props.formik.values, this.props.cityName);
-    if (city) {
-      this.loadAreas(city);
-    }
+    this.get().then(ret => this.setState({provinces: ret.data}, () => {
+      const province = getIn(this.props.formik.values, this.props.provinceName);
+      if (province) {
+        this.loadCities(province, () => {
+          const city = getIn(this.props.formik.values, this.props.cityName);
+          if (city) {
+            this.loadAreas(city);
+          }
+        });
+      }
+    }));
   }
 
   handleChangeProvince = (e) => {
@@ -43,14 +43,14 @@ class AreaCascading extends React.Component {
     this.loadAreas(e.target.value);
   };
 
-  loadCities(province) {
+  loadCities(province, cb) {
     return this.get(province).then(ret => {
-      this.setState({cities: ret.data, areas: []});
+      this.setState({cities: ret.data, areas: []}, cb);
     });
   }
 
-  loadAreas(city) {
-    return this.get(city).then(ret => this.setState({areas: ret.data}));
+  loadAreas(city, cb) {
+    return this.get(city).then(ret => this.setState({areas: ret.data}, cb));
   }
 
   get(value = '') {
