4aa9e7ceaa43982a3aaaf8a00b4739b29d62466b
twinh <twinhuang@qq.com>
2018-11-17 18:31:42 +0800

    feat: 完善表单操作

--- modules/components/bs4/FormAction.js
+++ modules/components/bs4/FormAction.js
@@ -4,13 +4,11 @@ import CListBtn from 'components/bs4/CListBtn';
 
 function FormAction({url, list = true}) {
   return (
-    <Form.Group as={Row} className="clearfix form-actions">
+    <Form.Group as={Row} className="form-actions">
       <Col sm={{offset: 2}}>
-        <Button type="submit">
-          提交
-        </Button>
-        &nbsp; &nbsp; &nbsp;
-        {list && (url ? <Button href={url}><i className="fa fa-undo bigger-110"/>{' '}返回列表</Button> : <CListBtn/>)}
+        <Button type="submit">提交</Button>
+        &nbsp; &nbsp; &nbsp;{/*todo bs4: m-l*/}
+        {list && (url ? <Button href={url} variant="secondary">返回列表</Button> : <CListBtn/>)}
       </Col>
     </Form.Group>
   );
