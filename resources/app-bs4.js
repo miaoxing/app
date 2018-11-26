62ca1dc593e5e026b6ccf5fef1877a8b19128cad
twinh <twinhuang@qq.com>
2018-11-26 17:35:13 +0800

    feat: 先移除无用功能

--- resources/app-bs4.js
+++ resources/app-bs4.js
@@ -1,13 +1,10 @@
 import jQuery from "jquery";
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // TODO 使用时引入
-import 'comps/bootstrap-mobile/dist/js/bootstrap-mobile.min';
 import 'vendor/miaoxing/app/public/js/app';
 import 'vendor/miaoxing/app/public/js/tips';
 import 'vendor/miaoxing/app/public/js/bootstrap-popup';
 import 'vendor/miaoxing/app/public/js/bootstrap-ajax-tips';
 import 'vendor/miaoxing/app/resources/styles/bootstrap.scss';
 import 'vendor/miaoxing/app/public/css/tips.css';
-import 'vendor/miaoxing/app/public/css/app.css';
 
 window.$ = window.jQuery = jQuery;
 $.tips.defaults.baseClass = 'badge';
