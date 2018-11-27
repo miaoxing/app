953230aa5f847cae043e7dacb34a2d086c6c127a
twinh <twinhuang@qq.com>
2018-11-27 18:21:59 +0800

    feat: 入口移除旧的tips

--- resources/app-bs4.js
+++ resources/app-bs4.js
@@ -1,10 +1,7 @@
 import jQuery from "jquery";
 import 'vendor/miaoxing/app/public/js/app';
-import 'vendor/miaoxing/app/public/js/tips';
 import 'vendor/miaoxing/app/public/js/bootstrap-popup';
 import 'vendor/miaoxing/app/public/js/bootstrap-ajax-tips';
 import 'vendor/miaoxing/app/resources/styles/bootstrap.scss';
-import 'vendor/miaoxing/app/public/css/tips.css';
 
 window.$ = window.jQuery = jQuery;
-$.tips.defaults.baseClass = 'badge';
