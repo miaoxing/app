351327f70c09242551b45435a2bf77a131281083
twinh <twinhuang@qq.com>
2018-11-29 10:35:47 +0800

    feat: 引入antd来简化代码

---
+++ modules/modal.js
@@ -0,0 +1,140 @@
+// From https://github.com/ant-design/ant-design/blob/master/components/modal
+
+import React from "react";
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
+// import ActionButton from './ActionButton';
+import {Button, Modal} from "react-bootstrap4";
+
+var IS_REACT_16 = !!ReactDOM.createPortal;
+
+const ConfirmDialog = (props) => {
+  const {onCancel, onOk, close, zIndex, afterClose, visible, keyboard, centered, getContainer, maskStyle, okButtonProps, cancelButtonProps} = props;
+  const iconType = props.iconType || 'question-circle';
+  const okType = props.okType || 'primary';
+  const prefixCls = props.prefixCls || 'ant-modal';
+  const contentPrefixCls = `${prefixCls}-confirm`;
+  // 默认为 true，保持向下兼容
+  const okCancel = ('okCancel' in props) ? !props.okCancel : true;
+  const width = props.width || 416;
+  const style = props.style || {};
+  // 默认为 false，保持旧版默认行为
+  const maskClosable = props.maskClosable === undefined ? false : props.maskClosable;
+  const runtimeLocale = {};
+  const okText = props.okText ||
+    (okCancel ? runtimeLocale.okText : runtimeLocale.justOkText);
+  const cancelText = props.cancelText || runtimeLocale.cancelText;
+  const autoFocusButton = props.autoFocusButton === null ? false : props.autoFocusButton || 'ok';
+
+  const classString = classNames(
+    contentPrefixCls,
+    `${contentPrefixCls}-${props.type}`,
+    props.className,
+  );
+
+  const cancelButton = okCancel && (
+    <Button actionFn={onCancel} closeModal={close} autoFocus={autoFocusButton === 'cancel'}
+      buttonProps={cancelButtonProps}>
+      {cancelText}
+    </Button>
+  );
+
+  return (
+    <Modal centered backdrop="static" show className="modal-prompt modal-zoom">
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer className="border-top">
+        <Button className="border-right text-dark">
+          取消
+        </Button>
+        <Button className="text-primary">
+          确认
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
+    /*<Dialog
+      prefixCls={prefixCls}
+      className={classString}
+      wrapClassName={classNames({ [`${contentPrefixCls}-centered`]: !!props.centered })}
+      onCancel={close.bind(this, { triggerCancel: true })}
+      visible={visible}
+      title=""
+      transitionName="zoom"
+      footer=""
+      maskTransitionName="fade"
+      maskClosable={maskClosable}
+      maskStyle={maskStyle}
+      style={style}
+      width={width}
+      zIndex={zIndex}
+      afterClose={afterClose}
+      keyboard={keyboard}
+      centered={centered}
+      getContainer={getContainer}
+    >
+      <div className={`${contentPrefixCls}-body-wrapper`}>
+        <div className={`${contentPrefixCls}-body`}>
+          <Icon type={iconType!} />
+          <span className={`${contentPrefixCls}-title`}>{props.title}</span>
+          <div className={`${contentPrefixCls}-content`}>{props.content}</div>
+        </div>
+        <div className={`${contentPrefixCls}-btns`}>
+          {cancelButton}
+          <ActionButton type={okType} actionFn={onOk} closeModal={close} autoFocus={autoFocusButton === 'ok'} buttonProps={okButtonProps}>
+            {okText}
+          </ActionButton>
+        </div>
+      </div>
+    </Dialog>*/
+  );
+};
+
+export default function confirm(config) {
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+
+  let currentConfig = {...config, close, visible: true};
+
+  function close(...args) {
+    currentConfig = {
+      ...currentConfig,
+      visible: false,
+      afterClose: destroy.bind(this, ...args),
+    };
+    if (IS_REACT_16) {
+      render(currentConfig);
+    } else {
+      destroy(...args);
+    }
+  }
+
+  function update(newConfig) {
+    currentConfig = {
+      ...currentConfig,
+      ...newConfig,
+    };
+    render(currentConfig);
+  }
+
+  function destroy(...args) {
+    const unmountResult = ReactDOM.unmountComponentAtNode(div);
+    if (unmountResult && div.parentNode) {
+      div.parentNode.removeChild(div);
+    }
+    const triggerCancel = args && args.length &&
+      args.some(param => param && param.triggerCancel);
+    if (config.onCancel && triggerCancel) {
+      config.onCancel(...args);
+    }
+  }
+
+  function render(props) {
+    ReactDOM.render(<ConfirmDialog {...props} />, div);
+  }
+
+  render(currentConfig);
+  return {
+    destroy: close,
+    update,
+  };
+}
