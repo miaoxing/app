90473f071d7654678bf2c7e35f8abf51c6befc73
twinh <twinhuang@qq.com>
2018-12-02 23:52:52 +0800

    feat: 合并admin和v1

--- modules/components/Loading.js
+++ modules/components/Loading.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default (props) => {
   if (props.error) {
-    console.log(props.error);
-    return <div className="m-t">很抱歉,加载出错! <button className="btn btn-default btn-sm" onClick={ props.retry }>重试</button>
+    if (props.error.code === 'MODULE_NOT_FOUND'
+      && document.getElementById('root').parentElement.children.length > 1) {
+      // 忽略页面输出内容的情况
+      return '';
+    }
+
+    return <div className="m-t">很抱歉,加载出错! <button className="btn btn-default btn-sm" onClick={props.retry}>重试</button>
       <div className="hide">{props.error.toString()}</div>
     </div>;
   } else {
