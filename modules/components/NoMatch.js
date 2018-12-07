32df33450a24742856bb69bd8fd49584157ac4fa
twinh <twinhuang@qq.com>
2018-12-07 14:41:47 +0800

    chore: 页面有输出内容则不显示404

--- modules/components/NoMatch.js
+++ modules/components/NoMatch.js
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default () => <div className="text-center hide">很抱歉,您访问的页面不存在,请检查后再试.</div>;
+export default () => {
+  if (document.getElementById('root').parentElement.children.length > 1) {
+    // 忽略页面输出内容的情况
+    return '';
+  }
+
+  return <div className="text-center">很抱歉,您访问的页面不存在,请检查后再试.</div>;
+}
