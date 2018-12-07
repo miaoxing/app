81b6cf6d86bba3c1090f2a62cf21262035ca7d38
twinh <twinhuang@qq.com>
2018-12-07 14:39:02 +0800

    chore: 先不显示404

--- modules/components/NoMatch.js
+++ modules/components/NoMatch.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div className="text-center">很抱歉,您访问的页面不存在,请检查后再试.</div>;
+export default () => <div className="text-center hide">很抱歉,您访问的页面不存在,请检查后再试.</div>;
