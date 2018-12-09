7e50bf3bee4a1e727b12d567a47a627a9d155274
twinh <twinhuang@qq.com>
2018-12-09 11:35:29 +0800

    feat: bs4合并到默认中

--- resources/pages/index-bs4.js
+++
@@ -1,7 +0,0 @@
-import '@babel/polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppContainerBs4 from './AppContainerBs4';
-import '../scss/app.scss';
-
-ReactDOM.render(<AppContainerBs4/>, document.getElementById('root'));
