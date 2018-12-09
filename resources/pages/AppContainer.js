1324aa807712044aa19073bd2c44b6aa5f8bbead
twinh <twinhuang@qq.com>
2018-12-09 22:08:12 +0800

    feat: 更新app加载

--- resources/pages/AppContainer.js
+++ resources/pages/AppContainer.js
@@ -5,8 +5,7 @@ import pages from 'data/cache/app-pages';
 
 class AppContainer extends React.Component {
   importPage(plugin, controller, action) {
-    console.log('dd', plugin,  controller, action);
-    return pages[`/${plugin}/resources/pages/${controller}/${action}.js`]();
+    return pages[`${plugin}/${controller}/${action}`]();
   }
 
   render() {
