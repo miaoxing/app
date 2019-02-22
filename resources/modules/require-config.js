0af589e16d93cef8c55299234d6510598af5d057
twinh <twinhuang@qq.com>
2019-02-22 14:10:23 +0800

    refactoring: form 使用完整路径

--- resources/modules/require-config.js
+++ resources/modules/require-config.js
@@ -3,9 +3,8 @@ window.requirejs.config({
   waitSeconds: 30,
   paths: {
     css: 'plugins/app/libs/require-css/css.min',
-    form: 'plugins/admin/js/form',
-
     // 保留兼容已有的缓存
+    form: 'plugins/admin/js/form',
     template: 'plugins/app/libs/artTemplate/template.min',
     'jquery-form': 'plugins/app/libs/jquery-form/jquery.form',
   }
