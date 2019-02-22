e2460711419dcb46a2e73d879ed6e6c19ba5a259
twinh <twinhuang@qq.com>
2019-02-22 14:04:00 +0800

    refactoring: template 使用完整路径

--- resources/modules/require-config.js
+++ resources/modules/require-config.js
@@ -4,9 +4,9 @@ window.requirejs.config({
   paths: {
     css: 'plugins/app/libs/require-css/css.min',
     form: 'plugins/admin/js/form',
-    template: 'plugins/app/libs/artTemplate/template.min',
 
     // 保留兼容已有的缓存
+    template: 'plugins/app/libs/artTemplate/template.min',
     'jquery-form': 'plugins/app/libs/jquery-form/jquery.form',
   }
 });
