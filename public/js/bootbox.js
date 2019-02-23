b6a0fe03bea9fe5aa538551507cf49bbf4b5e156
twinh <twinhuang@qq.com>
2019-02-23 12:47:18 +0800

    refactoring: 移除bootprompt，直接更新bootbox到5.0

--- public/js/bootbox.js
+++ public/js/bootbox.js
@@ -1,6 +1,11 @@
-define(['plugins/app/libs/bootbox/bootbox'], function(bootbox){
-    bootbox.setDefaults({
-        locale: 'zh_CN'
-    });
-    return bootbox;
+define(['plugins/app/libs/bootbox/bootbox'], function (bootbox) {
+  bootbox.addLocale('zh_CN', {
+    OK: 'OK',
+    CANCEL: '取消',
+    CONFIRM: '确认'
+  });
+  bootbox.setDefaults({
+    locale: 'zh_CN'
+  });
+  return bootbox;
 });
