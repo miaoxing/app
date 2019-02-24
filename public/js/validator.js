a95f85fabd960bb2460fa5eb26ba968c22d8b472
twinh <twinhuang@qq.com>
2019-02-24 10:01:36 +0800

    refactoring: 更新 validation 支持 bs4

--- public/js/validator.js
+++ public/js/validator.js
@@ -11,22 +11,18 @@ require.config({
 });
 
 define(['jquery-validation', 'jquery-validation-zh'], function () {
-  // http://stackoverflow.com/a/19006517
-  $.validator.setDefaults({
+  // https://stackoverflow.com/questions/52159649/jquery-validation-plugin-and-bootstrap-4
+  jQuery.validator.setDefaults({
     errorElement: 'span',
-    errorClass: 'form-text',
-    highlight: function (element) {
-      $(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function (element) {
-      $(element).closest('.form-group').removeClass('has-error');
-    },
     errorPlacement: function (error, element) {
-      if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
-        error.insertAfter(element.parent());
-      } else {
-        error.insertAfter(element);
-      }
+      error.addClass('invalid-feedback');
+      element.closest('.form-group').append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass('is-invalid');
     }
   });
 });
