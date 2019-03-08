c821bad62d02da2c6b8c2345009029b9658869f4
twinh <twinhuang@qq.com>
2019-03-08 20:31:39 +0800

    refactoring: FormText 兼容新版

--- resources/modules/components/FormText.js
+++ resources/modules/components/FormText.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {Col, FormLabel, FormControl, FormGroup} from 'react-bootstrap';
 
 const FormText = ({label, children}) => {
-  return <FormGroup>
-    <Col as={FormLabel} sm={2}>
+  return <FormGroup className="row">
+    <FormLabel column sm={2} className="text-sm-right">
       {label}
-    </Col>
+    </FormLabel>
     <Col sm={10}>
       <div className="form-control-plaintext">{children || '-'}</div>
     </Col>
