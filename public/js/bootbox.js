2cea7b922adc592f53cc21c8c3886a178478ce61
twinh <twinhuang@qq.com>
2019-03-09 17:07:22 +0800

    refactoring: 确认 =》 确定

--- public/js/bootbox.js
+++ public/js/bootbox.js
@@ -2,7 +2,7 @@ define(['plugins/app/libs/bootbox/bootbox'], function (bootbox) {
   bootbox.addLocale('zh_CN', {
     OK: 'OK',
     CANCEL: '取消',
-    CONFIRM: '确认'
+    CONFIRM: '确定'
   });
   bootbox.setDefaults({
     locale: 'zh_CN'
--- resources/modules/datatables/deletable.js
+++ resources/modules/datatables/deletable.js
@@ -8,7 +8,7 @@
 $.fn.dataTableExt.oApi.deletable = function (setting, linkSelector, message) {
   var that = this;
   linkSelector = linkSelector || '.js-delete-record';
-  message = message || '删除后将无法还原,确认删除?';
+  message = message || '删除后将无法还原,确定删除?';
   that.on('click', linkSelector, function () {
     var $link = $(this);
     $.confirm(message, function (result) {
