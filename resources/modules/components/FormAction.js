24a4cfb495749d4f6e3cc58932d8c9f51f34d8e2
twinh <twinhuang@qq.com>
2019-03-10 17:37:09 +0800

    refactoring: FormAction 更新

--- resources/modules/components/FormAction.js
+++ resources/modules/components/FormAction.js
@@ -4,11 +4,10 @@ import CListBtn from 'components/CListBtn';
 
 function FormAction({url, list = true}) {
   return (
-    <Form.Group as={Row} className="form-actions">
-      <Col sm={{offset: 2}}>
+    <Form.Group as={Row} className="form-actions px-0">
+      <Col sm={{span: 10, offset: 2}}>
         <Button type="submit">提交</Button>
-        &nbsp; &nbsp; &nbsp;{/*todo bs4: m-l*/}
-        {list && (url ? <Button href={url} variant="secondary">返回列表</Button> : <CListBtn/>)}
+        {list && (url ? <Button href={url} variant="secondary" className="ml-3">返回列表</Button> : <CListBtn/>)}
       </Col>
     </Form.Group>
   );
