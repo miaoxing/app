826b723b9d94554ea864435d3271a17ed8c73e0e
twinh <twinhuang@qq.com>
2019-03-11 16:28:04 +0800

    refactoring: 移除兼容的旧类

--- resources/modules/components/Loading.js
+++ resources/modules/components/Loading.js
@@ -9,11 +9,11 @@ export default (props) => {
     }
 
     $.log(props.error.toString(), {stack: props.error.stack});
-    return <div className="mt-3 mt-3">
+    return <div className="mt-3">
       很抱歉,加载出错! <button className="btn btn-secondary btn-sm" onClick={props.retry}>重试</button>
-      <div className="d-none hide">{props.error.toString()}</div>
+      <div className="d-none">{props.error.toString()}</div>
     </div>;
   } else {
-    return <div className="text-center mt-3 mt-3">努力加载中...</div>;
+    return <div className="text-center mt-3">努力加载中...</div>;
   }
 };
