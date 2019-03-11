cc5a9c024cd36cd7d10aa84ac2438b91c59064a7
twinh <twinhuang@qq.com>
2019-03-11 10:05:38 +0800

    refactoring: 页面指定才显示标题

--- resources/modules/components/PageHeader.js
+++ resources/modules/components/PageHeader.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 class PageHeader extends React.Component {
-  render () {
+  render() {
     return (
       <div className="page-header">
         {this.props.children && <div className="float-right">{this.props.children}</div>}
         <h1>
           {this.props.parentTitle || wei.page.controllerTitle}
-          <small>
-            <i className="fa fa-angle-double-right" />
-            {' '}{this.props.title || wei.page.actionTitle}
-          </small>
+          {this.props.title && <small>
+            <i className="fa fa-angle-double-right"/>
+            {' '}{this.props.title}
+          </small>}
         </h1>
       </div>
     );
