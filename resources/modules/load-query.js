ce549cf58d52a65ba1781f094b2ad7e58929b442
twinh <twinhuang@qq.com>
2019-03-17 21:18:28 +0800

    fix: 默认的resetForm会重置formik的值

--- resources/modules/load-query.js
+++ resources/modules/load-query.js
@@ -2,5 +2,7 @@ import 'jquery-unparam';
 import 'jquery-populate';
 
 export default $.fn.loadQuery = function () {
-  return this.populate($.unparam(location.search.substring(1)));
+  return this.populate($.unparam(location.search.substring(1)), {
+    resetForm: false,
+  });
 };
