04f26984471fe3148a8f8f4fd1a09d03c9f2b4ba
twinh <twinhuang@qq.com>
2019-03-26 11:18:23 +0800

    feat: Radio合并到FormCheck中

--- resources/modules/components/FormCheck.js
+++ resources/modules/components/FormCheck.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Form} from 'react-bootstrap';
-import {Field} from 'formik';
+import {Field, getIn} from 'formik';
 
 export default ({id, ...props}) => {
   if (!id) {
@@ -10,12 +10,12 @@ export default ({id, ...props}) => {
   return (
     <Field
       name={props.name}
-      render={({field, form, ...more}) => (
-        <>
-          {console.log(field, form, more)}
-          <Form.Check custom id={id} checked={form.values[props.name] == props.value} {...field} {...props}/>
-        </>
-      )}
+      render={({field, form}) => {
+        // Checked状态要自行维护
+        // @link https://github.com/jaredpalmer/formik/pull/1115
+        const checked = props.value == getIn(form.values, props.name);
+        return <Form.Check custom checked={checked} id={id} {...field} {...props}/>
+      }}
     />
   );
 }
--- resources/modules/components/Radio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Field from "components/Field";
-import {connect} from "formik";
-
-function Radio(props) {
-  return <Field type="radio" checked={props.formik.values[props.name] == props.value} {...props}/>;
-}
-
-export default connect(Radio);
