9bc4c304b0f47ad4c8fc8cececa103909f842531
twinh <twinhuang@qq.com>
2019-03-26 14:58:31 +0800

    feat: FormCheck 支持数组的情况

--- resources/modules/components/FormCheck.js
+++ resources/modules/components/FormCheck.js
@@ -12,9 +12,28 @@ export default ({id, ...props}) => {
       name={props.name}
       render={({field, form}) => {
         // Checked状态要自行维护 @link https://github.com/jaredpalmer/formik/pull/1115
-        // HTML会转换为字符串，因此不用严格比较
-        const checked = props.value == getIn(form.values, props.name);
-        return <Form.Check custom checked={checked} id={id} {...field} {...props}/>
+        let checked;
+        let extProps = {};
+
+        if (typeof field.value === 'object') {
+          // 多个checkbox选择的情况
+          checked = field.value.includes(props.value);
+          extProps.onChange = () => {
+            let nextValue;
+            if (checked) {
+              nextValue = field.value.filter(value => value !== props.value);
+            } else {
+              nextValue = field.value.concat(props.value);
+            }
+            form.setFieldValue(props.name, nextValue);
+          }
+        } else {
+          // 单个checkbox或radio的情况
+          // HTML会转换为字符串，因此不能用严格比较
+          checked = props.value == field.value;
+        }
+
+        return <Form.Check custom checked={checked} id={id} {...field} {...props} {...extProps}/>
       }}
     />
   );
