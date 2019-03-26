12b2fef02bc910e1815257eb94846b4ea36e46c0
twinh <twinhuang@qq.com>
2019-03-26 12:07:11 +0800

    feat: 替换掉 CheckboxV2

--- resources/modules/components/CheckboxV2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Field from "components/Field";
-import {connect, getIn} from "formik";
-
-function CheckboxV2(props) {
-  return <Field type="checkbox" checked={getIn(props.formik.values, props.name) == props.value} {...props}/>;
-}
-
-export default connect(CheckboxV2);
