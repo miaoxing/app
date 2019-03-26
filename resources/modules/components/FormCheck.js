1e92b7bc23400896c7c1c22c0835f4e070bc78e0
twinh <twinhuang@qq.com>
2019-03-26 11:03:03 +0800

    fix: 解绑无用户出错

--- resources/modules/components/FormCheck.js
+++ resources/modules/components/FormCheck.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {Form} from 'react-bootstrap';
+import {Field} from 'formik';
 
 export default ({id, ...props}) => {
   if (!id) {
     id = props.name + '-' + props.value;
   }
 
-  return (<Form.Check custom id={id} {...props}/>);
+  return (
+    <Field
+      name={props.name}
+      render={({field, form, ...more}) => (
+        <>
+          {console.log(field, form, more)}
+          <Form.Check custom id={id} checked={form.values[props.name] == props.value} {...field} {...props}/>
+        </>
+      )}
+    />
+  );
 }
