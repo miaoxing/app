e911f7351228327e9dc5b98e3b3adc2010910630
twinh <twinhuang@qq.com>
2019-04-01 12:05:48 +0800

    feat: 优化表格状态，解决重载时会重置回原来状态

--- resources/modules/components/TableStatusCheckbox.js
+++ resources/modules/components/TableStatusCheckbox.js
@@ -3,22 +3,45 @@ import {withTable} from "components/TableProvider";
 import app from "app";
 import {FormCheck} from "react-bootstrap";
 
+// 记录checkbox状态，以免被外部重置
+const store = {};
+
 @withTable
 export default class TableStatusCheckbox extends React.Component {
+  state = {
+    checked: this.getValue()
+  };
+
   handleChange = () => {
+    const checked = !this.state.checked;
+
+    // 更改时，将状态记录起来
+    store[this.props.row.id] = checked;
+
+    this.setState({checked: checked});
     app.post(this.props.url || app.actionUrl('update'), {
       id: this.props.row.id,
-      [this.props.name]: +!this.getValue(),
+      [this.props.name]: +checked,
     }).then(ret => {
       app.ret(ret);
+
+      // 重载数据，加载新的状态
       this.props.table.reload();
+
+      // 更改后,删除状态
+      delete store[this.props.row.id];
     });
   };
 
   getValue() {
+    // 如果有更改过，显示更改过的状态
+    if (typeof store[this.props.row.id] !== 'undefined') {
+      return store[this.props.row.id]
+    }
+
     let value = this.props.row[this.props.name];
     // 兼容数据库返回
-    return value === '0' ? 0 : value;
+    return value === '0' ? false : !!value;
   }
 
   render() {
@@ -26,7 +49,7 @@ export default class TableStatusCheckbox extends React.Component {
       custom
       label={' '}
       id={'table-status-toggle-' + this.props.row.id}
-      checked={this.getValue()}
+      checked={this.state.checked}
       onChange={this.handleChange}
     />
   }
