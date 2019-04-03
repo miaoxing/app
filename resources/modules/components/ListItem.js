bcab3ae117c70440d13087f987dc82b9462c60b4
twinh <twinhuang@qq.com>
2019-04-03 11:19:28 +0800

    feat: ListItem 支持快速设置 title detail text

--- resources/modules/components/ListItem.js
+++ resources/modules/components/ListItem.js
@@ -1,14 +1,25 @@
 import React from "react";
 import classNames from "classnames";
 import propTypes from 'prop-types';
+import ListTitle from "components/ListTitle";
+import ListText from "components/ListText";
+import ListDetail from "components/ListDetail";
 
 const ListItem = (props) => {
   const {
     as: Component,
     className,
+    title,
+    detail,
+    text,
+    children,
     description,
     ...rest
   } = props;
+
+  if (detail && text) {
+    throw new Error('detail 和 text 只能同时使用一个');
+  }
 
   return (
     <Component
@@ -18,7 +29,15 @@ const ListItem = (props) => {
         'list-item',
         description && 'list-description',
       )}
-    />
+    >
+      {children || (
+        <>
+          {title && <ListTitle>{title}</ListTitle>}
+          {detail && <ListDetail>{detail}</ListDetail>}
+          {text && <ListText>{text}</ListText>}
+        </>
+      )}
+    </Component>
   );
 };
 
