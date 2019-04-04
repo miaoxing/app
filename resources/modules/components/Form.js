ac35448bbc5aca8c9b46808ba74d37a732db3372
twinh <twinhuang@qq.com>
2019-04-04 10:39:37 +0800

    feat: Form自动识别url

--- resources/modules/components/Form.js
+++ resources/modules/components/Form.js
@@ -1,38 +1,41 @@
 import React from 'react';
-import {Form as RbForm} from 'react-bootstrap';
-import {Formik} from 'formik';
+import propTypes from 'prop-types';
+import {Formik, Form as FormikForm} from 'formik';
 import {withRouter} from 'react-router-dom';
 import app from 'app';
 import axios from 'axios';
 
+/**
+ * 在基础的表单上增加了
+ *
+ * 1. 点击提交数据发送到后台
+ * 2. 提交成功后跳转到相应页面
+ */
 class Form extends React.Component {
-  render() {
-    return (
-      <Formik
-        initialValues={this.props.initialValues}
-        enableReinitialize={true}
-        onSubmit={(values, actions) => {
-          axios({
-            url: this.props.url,
-            method: 'post',
-            data: values,
-            loading: true,
-          }).then((res) => {
-            app.ret(res.data, () => {
-              if (res.data.code === 1) {
-                this.redirect(this.getRedirectUrl());
-              }
-            });
-          });
-        }}
-        render={({handleSubmit, isSubmitting}) => (
-          <RbForm onSubmit={handleSubmit}>
-            {this.props.children}
-          </RbForm>
-        )}
-      />
-    );
-  }
+  static propTypes = {
+    /**
+     * 提交到后台的地址，默认自动识别为当前表单地址
+     */
+    url: propTypes.string,
+
+    /**
+     * 提交成功后跳转的地址，默认为上一级页面
+     */
+    redirectUrl: propTypes.string,
+  };
+
+  handleSubmit = (values) => {
+    axios({
+      url: this.getSubmitUrl(),
+      method: 'post',
+      data: values,
+      loading: true,
+    }).then(({data: ret}) => {
+      app.ret(ret).suc(() => {
+        this.redirect(this.getRedirectUrl());
+      });
+    });
+  };
 
   redirect(url) {
     if (url !== this.props.history.location.pathname) {
@@ -40,13 +43,33 @@ class Form extends React.Component {
     }
   }
 
+  getSubmitUrl() {
+    return this.props.url || app.curFormUrl();
+  }
+
   getRedirectUrl() {
-    // 自动跳转到上一页
     if (this.props.redirectUrl) {
       return this.props.redirectUrl;
     } else {
-      return this.props.url.substring(0, this.props.url.lastIndexOf('/'));
+      // 自动跳转到上一页
+      const url = this.getSubmitUrl();
+      return url.substring(0, url.lastIndexOf('/'));
     }
+  }
+
+  render() {
+    return (
+      <Formik
+        initialValues={this.props.initialValues}
+        enableReinitialize={true}
+        onSubmit={this.handleSubmit}
+        render={() => (
+          <FormikForm>
+            {this.props.children}
+          </FormikForm>
+        )}
+      />
+    );
   }
 }
 
