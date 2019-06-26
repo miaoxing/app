47d47b40e34247d236a1abcdb60f2357c48c3a49
twinh <twinhuang@qq.com>
2019-06-26 11:20:07 +0800

    fix: 解决传入包含 search 的路径导致解析错误

--- resources/modules/components/Link.js
+++ resources/modules/components/Link.js
@@ -1,28 +1,20 @@
 import React from "react";
 import {Link as RouterLink} from "react-router-dom";
 import {ModalContext} from 'components/ModalSwitch';
+import {createLocation} from 'history';
 
 export default function Link({to, modal, autoModal, ...props}) {
+  const location = createLocation(to, {modal});
+
   if (!autoModal) {
-    return <RouterLink
-      to={{
-        pathname: to,
-        state: {modal},
-      }}
-      {...props}
-    />
+    return <RouterLink to={location} {...props}/>
   }
 
   // 分开减少层级和逻辑
   return <ModalContext.Consumer>
     {({isModal}) => {
-      return <RouterLink
-        to={{
-          pathname: to,
-          state: {modal: modal || (isModal && autoModal)}
-        }}
-        {...props}
-      />
+      location.state.modal = modal || (isModal && autoModal);
+      return <RouterLink to={location} {...props}/>
     }}
   </ModalContext.Consumer>
 }
