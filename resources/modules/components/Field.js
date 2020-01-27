d62f7879d38eb0305e602da79921d1059605922c
twinh <twinhuang@qq.com>
2020-01-27 20:42:38 +0800

    chore: 指定 formik v1，v2 必须设置 initialValues

--- resources/modules/components/Field.js
+++ resources/modules/components/Field.js
@@ -4,6 +4,7 @@ import {connect, Field as FormikField, getIn, setIn} from 'formik';
 function Field(props) {
   // NOTE: 提交时,touch === undefined
   if (typeof getIn(props.formik.values, props.name) === 'undefined') {
+    // TODO formik v2 无效
     props.formik.values = setIn(props.formik.values, props.name, '');
   }
   return <FormikField {...props}/>
