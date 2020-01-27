8578d73026dbda4507a3160217aeb5015bfb381f
twinh <twinhuang@qq.com>
2020-01-27 23:50:54 +0800

    chore: Loading 内容更新

--- resources/modules/components/Loading.js
+++ resources/modules/components/Loading.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import {Spinner} from "react-bootstrap";
 
 export default (props) => {
-  return <div className="text-center mt-3">努力加载中...</div>;
+  return <div className="text-center my-5">
+    <Spinner animation="grow" variant="primary"/>
+    <div>努力加载中...</div>
+  </div>;
 };
