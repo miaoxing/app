f43e6171aa59fc9555c528392817a9bbf30a59b6
twinh <twinhuang@qq.com>
2020-01-29 16:33:52 +0800

    feat: 当页面不存在时，加载指定的控制器和方法

---
+++ src/Controller/App.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Miaoxing\App\Controller;
+
+use Miaoxing\Plugin\BaseController;
+
+class App extends BaseController
+{
+    public function indexAction()
+    {
+        return [];
+    }
+}
