26a0ee9bd2f6f3946b2726450054cc1ea98c03f9
twinh <twinhuang@qq.com>
2020-02-03 21:23:15 +0800

    feat(table): 增加 onChange 时，记录 sorter，请求参数带上 sorter

--- resources/modules/components/AntTable.js
+++ resources/modules/components/AntTable.js
@@ -1,30 +1,49 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import ProTable from '@ant-design/pro-table';
 import axios from "axios";
+import app from 'app';
 
-export default class extends React.Component {
-  render() {
-    const {url, ...restProps} = this.props;
+let querySorter = {};
 
-    return (
-      <ProTable
-        request={(params) => {
-          return new Promise(resolve => {
-            axios.get(url).then(({data}) => {
-              resolve(data)
-            });
-          });
-        }}
-        options={false}
-        search={false}
-        rowKey="id"
-        toolBarRender={false}
-        onChange={(pagination, filters, sorter, extra) => {
-          this.ref.current.reload();
-          console.log('params', pagination, filters, sorter, extra);
-        }}
-        {...restProps}
-      />
-    );
+const getSortPrams = () => {
+  // 取消排序后，field 是点击的字段，order 是 undefined
+  // 因此判断 order 无值则无需排序
+  if (!querySorter.order) {
+    return {};
   }
+
+  let {field: sort, order} = querySorter;
+  order = order === 'ascend' ? 'asc' : 'desc';
+
+  return  {sort, order};
+};
+
+export default ({url, ...restProps}) => {
+  const ref = useRef();
+
+  return (
+    <ProTable
+      actionRef={ref}
+      request={({current: page, pageSize: rows, ...params}) => {
+        return new Promise(resolve => {
+          const fullUrl = app.appendUrl(url, {page, rows, ...getSortPrams(), ...params});
+          axios.get(fullUrl).then(({data}) => {
+            resolve(data)
+          });
+        });
+      }}
+      onRequestError={e => {
+        throw e;
+      }}
+      options={false}
+      search={false}
+      rowKey="id"
+      toolBarRender={false}
+      onChange={(pagination, filters, sorter, extra) => {
+        querySorter = sorter;
+        ref.current.reload();
+      }}
+      {...restProps}
+    />
+  );
 }
