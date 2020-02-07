3f500fa735dcb542a82a303ed7c2476670e4f1e5
twinh <twinhuang@qq.com>
2020-02-07 20:39:07 +0800

    refactor: 更改 404 页面

--- resources/modules/components/NoMatch.js
+++ resources/modules/components/NoMatch.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {Result} from "antd";
+import {BtnLink} from "@miaoxing/button";
 
-export default () => {
-  return <div className="text-center">很抱歉,您访问的页面不存在,请检查后再试.</div>;
-}
+export default () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="很抱歉，您访问的页面不存在，请检查后再试。"
+    extra={<BtnLink to="/">返回首页</BtnLink>}
+  />
+);
