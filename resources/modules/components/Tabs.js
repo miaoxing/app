d6ad04eaa17431538afd1d16049d2893fe25c410
twinh <twinhuang@qq.com>
2020-02-13 11:56:24 +0800

    feat: 拆分出 tabs 相关组件

--- resources/modules/components/Tabs.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {Tabs as RbTabs} from 'react-bootstrap';
-import classNames from 'classnames';
-
-export default ({className, ...props}) => {
-  return <RbTabs className={classNames(className, 'nav-underline header-nav')} {...props}/>
-}
