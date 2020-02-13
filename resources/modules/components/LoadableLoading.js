96541b31286b29726cc0e827a73fa941cbe14f9b
twinh <twinhuang@qq.com>
2020-02-13 18:12:04 +0800

    refactor: 更新 loadable 出错页面

--- resources/modules/components/LoadableLoading.js
+++ resources/modules/components/LoadableLoading.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {PageLoading} from '@miaoxing/loading';
 import * as Sentry from '@sentry/browser';
+import {Result, Button} from 'antd';
 
 export default (props) => {
   if (props.error) {
     Sentry.captureException(props.error);
-    return <div className="mt-3">
-      很抱歉,加载出错! <button className="btn btn-secondary btn-sm" onClick={props.retry}>重试</button>
-      <div className="d-none">{props.error.toString()}</div>
-    </div>;
-  } else {
-    return <PageLoading/>;
+    return <Result
+      status="500"
+      title="出错了"
+      subTitle="很抱歉，加载出错！"
+      extra={<Button type="primary" onClick={props.retry}>重试</Button>}
+    />
   }
+  
+  return <PageLoading/>;
 };
