d0f44a8adb9603fce894a64680939d0f4e36f1ab
twinh <twinhuang@qq.com>
2020-02-13 17:47:28 +0800

    refactor: 前端出错改用 Sentry 上报

--- resources/modules/components/LoadableLoading.js
+++ resources/modules/components/LoadableLoading.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import {PageLoading} from '@miaoxing/loading';
+import * as Sentry from '@sentry/browser';
 
 export default (props) => {
   if (props.error) {
-    if (props.error.code === 'MODULE_NOT_FOUND'
-      && document.getElementById('root').parentElement.children.length > 1) {
-      // 忽略页面输出内容的情况
-      return '';
-    }
-
-    $.log(props.error.toString(), {stack: props.error.stack});
+    Sentry.captureException(props.error);
     return <div className="mt-3">
       很抱歉,加载出错! <button className="btn btn-secondary btn-sm" onClick={props.retry}>重试</button>
       <div className="d-none">{props.error.toString()}</div>
