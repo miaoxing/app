9bcec4024c015d73f11e3aec5815a7cfd51d8dac
twinh <twinhuang@qq.com>
2020-02-19 18:06:02 +0800

    chore: 默认页面可以访问，由接口控制权限

--- src/Controller/App.php
+++ src/Controller/App.php
@@ -6,6 +6,10 @@ use Miaoxing\Plugin\BaseController;
 
 class App extends BaseController
 {
+    protected $guestPages = [
+        'app'
+    ];
+
     public function indexAction()
     {
         return [];
