da7a0feab6e62c6c4380b5b6cdb6ff1932345895
twinh <twinhuang@qq.com>
2020-02-23 16:04:27 +0800

    refactor: 移除 setting，已有功能调整为 config

--- docs/AutoComplete.php
+++ docs/AutoComplete.php
@@ -5,9 +5,6 @@ namespace MiaoxingDoc\App {
     /**
      * @property    \Miaoxing\App\Service\Logger $logger
      * @method      bool logger($level, $message, $context = []) Logs with an arbitrary level
-     *
-     * @property    \Miaoxing\App\Service\Setting $setting 设置
-     * @method      string|\Miaoxing\App\Service\Setting|\Miaoxing\App\Service\Setting[] setting($id = null, $default = null)
      */
     class AutoComplete
     {
@@ -25,7 +22,4 @@ namespace {
 
     /** @var Miaoxing\App\Service\Logger $logger */
     $logger = wei()->logger;
-
-    /** @var Miaoxing\App\Service\Setting $setting */
-    $setting = wei()->setting;
 }
