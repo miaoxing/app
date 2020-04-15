0b770b4a236ad8d26ce240b6c2a2f33f14d1d75f
twinh <twinhuang@qq.com>
2020-04-15 10:39:30 +0800

    chore: 增加代码提示文件

---
+++ docs/auto-completion-dynamic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Miaoxing\App\Service;
+
+class JsConfig extends \Miaoxing\Plugin\BaseService
+{
+    /**
+     * @return array
+     * @see JsConfig::toArray
+     */
+    public function toArray()
+    {
+    }
+}
