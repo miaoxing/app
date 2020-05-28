d1c7bbb61d4bff5618bcc7d7fc0aa72615358b08
twinh <twinhuang@qq.com>
2020-05-28 15:13:29 +0800

    feat: 将 theme 中主要变量转换为 bootstrap sass 变量

--- resources/components/AppContainer.js
+++ resources/components/AppContainer.js
@@ -2,7 +2,7 @@ import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import App from "./App";
 import configs from 'data/configs/app';
-import theme from '@miaoxing/style/theme';
+import theme from '@miaoxing/style/theme-preset';
 
 const AppContainer = () => {
   return (<App theme={theme} {...configs}/>);
