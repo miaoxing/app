d555ccb58528b07e91f73672f7b2689a5d5b0df2
twinh <twinhuang@qq.com>
2020-05-29 21:39:46 +0800

    chore: app 更改 webpack 注入变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const WebpackConfig = require('@miaoxing/webpack');
+const {generateAntdVars, generateBootstrapVars} = require('@miaoxing/style/utils');
+
+const theme = require('@miaoxing/style/theme-preset');
+const name = path.basename(__dirname);
 
 module.exports = WebpackConfig.build({
-  name: path.basename(__dirname),
+  name,
   manifest: true,
+  lessLoaderOptions: {
+    lessOptions: {
+      modifyVars: generateAntdVars(theme),
+      javascriptEnabled: true,
+    },
+  },
+  sassLoaderOptions: {
+    prependData: function () {
+      return generateBootstrapVars(theme) + `@import "plugins/${name}/resources/scss/config";`;
+    }
+  },
   getEntries() {
     return {
-      [this.name] : `${this.rootDir}/plugins/${this.name}/resources/pages/index.js`
+      [name]: `${this.rootDir}/plugins/${name}/resources/pages/index.js`
     }
   }
 });
