960eb64dd5878d4cf2316f0d86e3d63a42ea6336
twinh <twinhuang@qq.com>
2020-06-07 16:35:22 +0800

    refactor: data => storage，兼容 laravel 的目录结构

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -1,7 +1,7 @@
 import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import App from "./App";
-import configs from 'data/configs/app';
+import configs from 'storage/configs/app';
 import theme from '@miaoxing/style/theme-preset';
 
 const AppContainer = () => {
