731ed1336e0fb2e5f350e7af66c832f6d96364f7
twinh <twinhuang@qq.com>
2020-06-10 11:01:36 +0800

    test: 增加 AppController 测试

---
+++ tests/Controller/AppControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MiaoxingTest\App\Controller;
+
+use Miaoxing\Plugin\Test\BaseControllerTestCase;
+
+class AppControllerTest extends BaseControllerTestCase
+{
+    public function testIndexAction()
+    {
+        $res = wei()->tester('app')->exec()->response();
+        $this->assertStringContainsString('<div id="root"></div>', $res);
+    }
+}
