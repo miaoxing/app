837247ae4a0454cbf0eb96bc19287d2b481fa812
twinh <twinhuang@qq.com>
2021-11-19 10:03:01 +0800

    feat(app): `$.ret` 提示改为 antd 的 message 组件

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -3,20 +3,46 @@ import $, {Ret} from 'miaoxing';
 import modal from '@mxjs/modal';
 import '@mxjs/modal/style/index.scss';
 
-import message from 'react-bootstrap-message/js/react-bootstrap-message';
+import {message} from 'antd';
+import message2 from 'react-bootstrap-message/js/react-bootstrap-message';
 import 'react-bootstrap-message/scss/react-bootstrap-message.scss';
 
 import axios from '@mxjs/axios';
 
 import {req, url} from '@mxjs/app';
 
-$.loading = message.loading;
+$.loading = message2.loading;
 $.alert = (message, fn) => modal.alert(message).then(fn);
 $.confirm = (message, fn) => modal(message).then(fn);
 
-$.ret = message.ret;
 $.suc = message.success;
-$.err = message.danger;
+$.err = message.error;
+$.ret = (ret, duration, callback) => {
+  const result = $[ret.code === 0 ? 'suc' : 'err'](ret.message, duration, callback);
+
+  let suc;
+  result.suc = fn => {
+    suc = fn;
+    return result;
+  };
+
+  let err;
+  result.err = fn => {
+    err = fn;
+    return result;
+  };
+
+  result.then(() => {
+    if (ret.code === 0 && suc) {
+      suc();
+    }
+    if (ret.code !== 0 && err) {
+      err();
+    }
+  });
+
+  return result;
+};
 
 $.http = (...args) => axios(...args).then(res => {
   res.ret = new Ret(res.data);
