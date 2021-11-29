0b6cfda0be7edad41aef427bd7406939ceb99ff8
twinh <twinhuang@qq.com>
2021-11-29 14:27:04 +0800

    refactor(app): 移除 bootstrap 变量
    BRAKING CHANGE: 移除 bootstrap 变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const path = require('path');
 const WebpackConfig = require('@mxjs/webpack');
-const {generateAntdVars, generateBootstrapVars} = require('@mxjs/style/utils');
+const {generateAntdVars} = require('@mxjs/style/utils');
 
 const theme = require('@mxjs/style/theme-preset');
 const name = 'index';
@@ -13,10 +12,6 @@ module.exports = WebpackConfig.build({
       modifyVars: generateAntdVars(theme),
       javascriptEnabled: true,
     },
-  },
-  sassLoaderOptions: {
-    // NOTE: 使用回调（即使返回值为空），实现使用 dart-sass 时，js 中加载的 sass 会消失不见
-    additionalData: generateBootstrapVars(theme) + `@import "plugins/app/scss/config";`,
   },
   getEntries() {
     return {
