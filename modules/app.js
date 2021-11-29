64256bdf99d000fea5c2544b3c39285ca3829e90
twinh <twinhuang@qq.com>
2021-11-29 14:44:42 +0800

    feat(app): 启动时加载配置

--- modules/app.js
+++ modules/app.js
@@ -4,6 +4,11 @@ import AppContainer from '../components/AppContainer';
 import './bootstrap';
 import '../scss/index.scss';
 
+import {wei} from '@mxjs/app';
+import config from 'config';
+
+wei.setConfigs(config);
+
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<AppContainer/>, root);
