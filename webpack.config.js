5f2b4902e6030312857fddaa76681e50fb6f2525
twinh <twinhuang@qq.com>
2022-06-14 09:20:02 +0800

    refactor: `lessLoaderOptions` 改为直接修改 webpack 配置对象

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const WebpackConfig = require('@mxjs/webpack');
 const {generateAntdVars} = require('@mxjs/style/utils');
-
 const theme = require('@mxjs/style/theme-preset');
 
-module.exports = WebpackConfig.build({
+const config = WebpackConfig.build({
   name: 'index',
   entry: 'plugins/app/modules/app.js',
-  lessLoaderOptions: {
-    lessOptions: {
-      modifyVars: generateAntdVars(theme),
-      javascriptEnabled: true,
-    },
-  },
 });
+
+config.module.rules.map(rule => {
+  rule.use && rule.use.map(use => {
+    if (use.loader === 'less-loader') {
+      use.options = {
+        lessOptions: {
+          modifyVars: generateAntdVars(theme),
+          javascriptEnabled: true,
+        },
+      };
+    }
+  });
+});
+
+module.exports = config;
