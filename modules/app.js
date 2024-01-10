da6a7b269e19f3dfa51e6886c4896e0807a36ebf
twinh <twinhuang@qq.com>
2024-01-10 21:36:21 +0800

    feat: 更新调用方法为 `react` 18
    BREAKING CHANGE: 更新调用方法为 `react` 18

--- modules/app.js
+++ modules/app.js
@@ -1,15 +1,14 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import AppContainer from '../components/AppContainer';
 
 import './bootstrap';
 import '../scss/index.scss';
 
-import {wei} from '@mxjs/app';
+import { wei } from '@mxjs/app';
 import config from 'config';
 
 wei.setConfigs(config);
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<AppContainer/>, root);
-}
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppContainer/>);
