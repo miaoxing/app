aeafd1fcc4d681972d015b8278b7a15e57edbbee
twinh <twinhuang@qq.com>
2024-05-16 23:11:12 +0800

    feat(app): `RouterStore `增加 `location` 变量

--- components/RouterStore.js
+++ components/RouterStore.js
@@ -1,14 +1,17 @@
 import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom';
 
 let navigate;
+let location;
 
 /**
  * @experimental
  */
 const RouterStore = () => {
   navigate = useNavigate();
+  location = useLocation();
 };
 
 export default RouterStore;
 
-export { navigate };
+export { navigate, location };
