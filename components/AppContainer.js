db8cce23a8dd9e4082d683298864000ba733af6b
twinh <twinhuang@qq.com>
2024-05-26 23:19:28 +0800

    refactor(app): 移除未使用的前端功能
    BREAKING CHANGE: 移除未使用的前端功能

--- components/AppContainer.js
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import configs from 'storage/configs/app';
-import theme from '@mxjs/style/theme-preset';
-
-const AppContainer = () => {
-  return (<App configs={{theme, ...configs}}/>);
-};
-
-export default AppContainer;
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const WebpackConfig = require('@mxjs/webpack');
-
-const config = WebpackConfig.build({
-  name: 'index',
-  entry: 'plugins/app/modules/app.js',
-});
-
-module.exports = config;
